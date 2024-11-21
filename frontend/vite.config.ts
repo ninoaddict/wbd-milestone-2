08437285a09f8f901a931ae198be82afc3e6d09e
ninoaddict <adrilbless37@gmail.com>
2024-11-21 23:36:30 +0700

    feat: add shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
     host: "0.0.0.0",
     strictPort: true,
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
