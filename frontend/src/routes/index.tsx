919a6ce3d6b59285414eadacab2c10e3b50e26e9
ninoaddict <adrilbless37@gmail.com>
2024-11-26 15:16:40 +0700

    fix: register endpoint in service

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/")({
 
 function HomeComponent() {
   return (
-    <div className="p-2">
+    <div className="p-2 min-h-screen bg-[#f4f2ee]">
       <h3>Welcome Home!</h3>
     </div>
   );
